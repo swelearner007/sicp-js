efb4fe6ac88a870ab09abe7409f0fe3ce23d2e64
Ashish Mittal <swelearner007@gmail.com>
2024-10-04 18:12:28 +0530

    Add solution to 1.9

---
+++ solution/chapter-1/1_9.js
@@ -0,0 +1,29 @@
+/*
+First - 
+inc 
+inc inc
+inc inc inc
+inc inc inc inc 
+inc inc inc
+inc inc
+inc
+This is recursive process
+
+*/
+
+/*
+Second one
+plus(4, 5);
+
+plus(dec(4), inc(5));
+plus(3, 6);
+plus(dec(3), inc(6));
+plus(2, 7);
+plus(dec(2), inc(7));
+plus(1, 8);
+plus(dec(1), inc(8));
+plus(0, 9);
+9;
+This is iterative. We can directly return the last value to first call and skip all calls in between.
+This is called tail recursion optimization that some compilers perform.
+*/
