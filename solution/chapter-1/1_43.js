d9222c79462ba5baad69cbd6314efc1fe156d698
Ashish Mittal <swelearner007@gmail.com>
2024-10-11 10:20:29 +0530

    Solve 1.43

---
+++ solution/chapter-1/1_43.js
@@ -0,0 +1,14 @@
+function repeated(f, n) {
+    if(n === 1) {
+        return x => f(x);
+    }
+
+    if(n > 0)
+        return x => f(repeated(f, n-1)(x));
+}
+
+function square(x) {
+    return x * x;
+}
+
+console.log(repeated(square, 2)(5));
